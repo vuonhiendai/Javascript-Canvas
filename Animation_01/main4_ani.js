093b50579e8c9c95dc3e02cc1d2e469f290a5da1
QUOC VIET LUONG <vuonhiendai@gmail.com>
2021-12-14 17:42:57 +0900

    commit 4

--- Animation_01/main4_ani.js
+++ Animation_01/main4_ani.js
@@ -1,7 +1,10 @@
 var context = document.getElementById("asteroids").getContext("2d");
 context.strokeStyle = "white";
 context.lineWidth = 1.5;
-var x = 0, y = context.canvas.height / 2;
+var x = 0, y = context.canvas.height / 2; radius = 20;
+let xspeed = 1.5, yspeed = 0, gravity = 0.1;
+let mouth = 0;
+
 function frame() {
     context.clearRect(0, 0, context.canvas.width, context.
     canvas.height);
@@ -10,7 +13,19 @@ function frame() {
 }
 
 function update() {
-    x += 1;
+    x += xspeed; y += yspeed;
+    yspeed += gravity;
+    if(y>=context.canvas.height - radius){
+        y = context.canvas.height - radius;
+        yspeed *= -0.6;
+        xspeed *= 0.95;
+
+    }
+    if(x<=0 || x>= context.canvas.width){
+        x = (x + context.canvas.width)% context.canvas.width;
+
+    }
+    mouth = Math.abs(Math.sin(6*Math.PI*x/(context.canvas.width)));
 }
     
 function draw(ctx) {
